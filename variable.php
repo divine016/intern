5948e152929b556e7ba890a0719b996a16604a8c
Kouti Divine <97065978+divine016@users.noreply.github.com>
2024-03-27 13:56:16 +0100

    learningabout variables in php

---
+++ variable.php
@@ -0,0 +1,40 @@
+<?php
+
+    //strings
+    $name = "kouts CQ";
+    $food = "poulet DG";
+    $email = "fake@gmail.com";
+
+    //intergers
+    $age = 21;
+    $users = 3;
+
+    //float
+    $gpa = 2.5;
+    $price = 4.99;
+
+
+    //bolean values are used in a loop andare mostly used internally.
+    $employed = false;
+    $online = true;
+    $for_sale = true;
+
+    //just an ecxmaple
+    /*
+    echo "hello i am {$name} <br>";
+    echo "and i love {$food} <br>";
+    echo "your email is {$email} <br>";
+
+    echo "you are {$age} years old <br>";  
+    echo "there are {$users} users online <br>";
+
+    echo "your gpa ps {$gpa} <br>";
+    echo "your food is \${$price} <br>";
+
+    echo "online status: {$online} <br>";
+    echo "employment status: {$employed} <br>"; */
+
+    //adding two intergers
+
+
+?>
